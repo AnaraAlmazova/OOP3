8c2f36fa8e5331fae0183ac74ef936ed55834a58
Анара Алмазова <anuyka.almaz@gmail.com>
2022-10-06 09:04:38 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+    String name;
+    int dateOfBirth;
+
+    public void getAge(){
+        System.out.println(2022-dateOfBirth);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	Person person=new Person();
+    person.name="Anara";
+    person.dateOfBirth=1993;
+
+    person.getAge();   //жылымды жазып. жашымды консолдон чыгаруу
+    }
+}
